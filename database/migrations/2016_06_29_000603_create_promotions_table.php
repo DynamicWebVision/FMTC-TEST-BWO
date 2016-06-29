80adc0dcf47b1eaa30f773ea4bc47b5a839d27f2
DynamicWebVision <dynamicwebvision@gmail.com>
2016-06-29 15:10:17 -0500

    Allowed Some Promotion Table Columns to be Null

--- database/migrations/2016_06_29_000603_create_promotions_table.php
+++ database/migrations/2016_06_29_000603_create_promotions_table.php
@@ -15,13 +15,13 @@ class CreatePromotionsTable extends Migration
         Schema::create('promotions', function (Blueprint $table) {
             $table->increments('id');
             $table->bigInteger('coupon_id');
-            $table->string('label', 150);
-            $table->string('merchant', 150);
-            $table->string('coupon_code', 20);
-            $table->string('logo_url', 100);
-            $table->dateTime('promo_start_dt');
-            $table->dateTime('promo_end_dt');
-            $table->dateTime('last_refresh');
+            $table->string('label', 150)->nullable();
+            $table->string('merchant', 150)->nullable();
+            $table->string('coupon_code', 20)->nullable();
+            $table->string('logo_url', 100)->nullable();
+            $table->dateTime('promo_start_dt')->nullable();
+            $table->dateTime('promo_end_dt')->nullable();
+            $table->dateTime('last_refresh')->nullable();
             $table->timestamps();
         });
     }
